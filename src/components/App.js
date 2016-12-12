265cf4d784b0037776ebdaf9ea83e22b3a8a160b
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-12-12 17:10:55 -0500

    updated with click handler function

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
+import style from './App.scss'
 
 class App extends Component {
 
+  constructor () {
+    super()
+    this.state = {
+      drops: [
+        { x: 5,
+          y: 10 },
+        { x: 25,
+          y: 105 }
+      ]
+    }
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => this.animate(), 1000 / 30)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  clickHandler () {
+    // Use this.setState to add a new item into drops { x: random, y: 0 }
+    this.setState = (this.drops)
+    console.log('Clicked!')
+  }
+
+  animate () {
+    // Use this.state.drops.map to return a new array of modified positions (e.g. y + 1)
+    // then set that array with this.setState({ drops: thatArray })
+  }
+
   render () {
+    const drops = this.state.drops.map((drop, i) => {
+      return <div className={style.drop} style={{ left: drop.x + 'px', top: drop.y + 'px' }} key={i} />
+    })
+
     return <div>
-      <h1>Hello, World!</h1>
+      <button className={style.rain} onClick={() => this.clickHandler()}>Make it Rain!</button>
+      <div className={style.drops}>
+        {drops}
+      </div>
     </div>
   }
 }
